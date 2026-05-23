1f52dc28976974e97dd0cc6e2e8d4586edda7bff
jonflores27 <jonfloresmartinez27@gmail.com>
2026-05-23 00:25:21 -0700

    Git hub commit

--- R/load_data.R
+++ R/load_data.R
@@ -1,3 +1,10 @@
+#' Title
+#'
+#' @returns A tibble
+#' @export
+#'
+#' @examples
+#' load_data()
 load_data <- function(){
   readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2021/2021-01-26/plastics.csv')
 
